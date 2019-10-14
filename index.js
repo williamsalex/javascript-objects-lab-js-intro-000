8ef6b8cad55849c98c55fb102ab6dbd2118eab7e
Alexander Williams <williams_alex@protonmail.com>
2019-10-14 14:05:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(objects, key, value) {
+  let obj = Object.assign({}, objects, {key:value})
+  obj
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
